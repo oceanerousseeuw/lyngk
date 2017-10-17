e41a14369af5c04b047489cbe5eb0791ef670c3a
oceane <oceane@debian.home>
2017-10-17 14:26:45 +0200

    story7_code_ok

--- src/Intersection.js
+++ src/Intersection.js
@@ -3,4 +3,10 @@
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
+    // soit c une couleur de pion
+    this.state = function(){
+        if(c==""){
+            return Lyngk.State.VACANT;
+        }
+    }
 };
